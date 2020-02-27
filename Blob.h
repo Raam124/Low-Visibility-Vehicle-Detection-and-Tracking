3500d036590db6ad5de9c54401dcd93fbba8acbe
Your Name <ramvijay124@gmail.com>
2020-02-27 17:51:21 +0530

    Low-Visibility-Vehilcle-Detection

---
+++ Blob.h
@@ -0,0 +1,36 @@
+#pragma once
+// Blob.h
+
+#ifndef MY_BLOB
+#define MY_BLOB
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+class Blob {
+public:
+    std::vector<cv::Point> currentContour;
+
+    cv::Rect currentBoundingRect;
+
+    std::vector<cv::Point> centerPositions;
+
+    double dblCurrentDiagonalSize; 
+    double dblCurrentAspectRatio;
+
+    bool blnCurrentMatchFoundOrNewBlob;
+
+    bool blnStillBeingTracked;
+
+    int intNumOfConsecutiveFramesWithoutAMatch;
+
+    cv::Point predictedNextPosition;
+
+    Blob(std::vector<cv::Point> _contour);
+    void predictNextPosition(void);
+
+};
+
+#endif    
+ 
